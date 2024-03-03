8c0850086d5ef023d7883217696621cb927d3f7b
psr261004 <psr261004@gmail.com>
2024-03-03 14:23:23 +0530

    vehicle

---
+++ question9/src/question9/Main.java
@@ -0,0 +1,15 @@
+package question9;
+
+//Main class
+public class Main {
+public static void main(String[] args) {
+   // Create an object of Car class
+   Car myCar = new Car(80, "Toyota");
+
+   // Call the drive method
+   myCar.drive();
+}
+}
+
+
+
